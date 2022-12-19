141ac1f28e264139c3dbbfe90fbed4af3731938b
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-12-19 18:42:10 -0300

    Criando ExternalLink e finalizando estilo do Profile

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,3 +1,7 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ExternalLink } from '../../../../components/ExternalLink'
 import { ProfileContainer, ProfileDetails, ProfilePicture } from './styles'
 
 export function Profile() {
@@ -7,7 +11,27 @@ export function Profile() {
       <ProfileDetails>
         <header>
           <h1>Wagner Ferreira Costa</h1>
+          <ExternalLink text="github" href="#" />
         </header>
+        <p>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </p>
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+            wagaofc
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBuilding} />
+            Zema
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUserGroup} />
+            1234 seguidores
+          </li>
+        </ul>
       </ProfileDetails>
     </ProfileContainer>
   )

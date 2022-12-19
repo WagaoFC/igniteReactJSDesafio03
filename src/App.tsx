0796bbb19c54f4b11c823418557aced13e62a7a8
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-12-19 11:08:54 -0300

    finalizando base

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-export function App() {
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/theme/defaultTheme'
 
+export function App() {
   return (
-    <h1>Hello World!</h1>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
